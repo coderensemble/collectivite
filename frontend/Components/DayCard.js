5fb5c9f529d663636c0d0c595d37c5c86e40c7d5
Adrien NEYRON <adrien.neyron@gmail.com>
2024-01-15 17:30:39 +0100

    start day card

--- frontend/Components/DayCard.js
+++ frontend/Components/DayCard.js
@@ -1,25 +1,41 @@
-import {
-    StyleSheet,
-    View,
-    Text,
-  } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 
-  export function DayCard() {
-    return(
+export function DayCard() {
+  const test = [
+    { heureActiviteMat: { heure: 8, minutes: 30 }, activiteMat: "Football" },
+    { repas: true },
+    { heureActiviteAm: { heure: 18, minutes: 30 }, activiteAm: "Peinture" },
+  ];
+
+  return (
     <View style={styles.container}>
-    <Text style={styles.title}> Aujourd'hui </Text>
-    <Text></Text>
+      <Text style={styles.title}> Aujourd'hui </Text>
+      {test.map((data, index) => (
+        <View key={index}>
+          {data.heureActiviteMat && (
+            <Text>
+              {data.heureActiviteMat.heure}:{data.heureActiviteMat.minutes} - {data.activiteMat}
+            </Text>
+          )}
+          {data.repas && <Text>Repas</Text>}
+          {data.heureActiviteAm && (
+            <Text>
+              {data.heureActiviteAm.heure}:{data.heureActiviteAm.minutes} - {data.activiteAm}
+            </Text>
+          )}
+        </View>
+      ))}
     </View>
-    )
-  };
+  );
+}
 
-  const styles = StyleSheet.create({
-    container:{
-        padding: 20,
-        backgroundColor: "#FFFF",
-        borderRadius: 20
-    },
-    title:{
-        textAlign: "center",
-    },
-  })
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    backgroundColor: "#FFFF",
+    borderRadius: 20,
+  },
+  title: {
+    textAlign: "center",
+  },
+});
